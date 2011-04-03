02ad69e571f8d410fed22141490e05fe352e49ab
Rayner Lucas <raynerluc@cpan.org>
2011-04-03 14:54:31 +0000

    Import of RAYNERLUC/Finance-Bank-Halifax-Sharedealing-0.03 from CPAN.
    gitpan-cpan-distribution: Finance-Bank-Halifax-Sharedealing
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         RAYNERLUC/Finance-Bank-Halifax-Sharedealing/Finance-Bank-Halifax-Sharedealing-0.03.tar.gz
    gitpan-cpan-author:       RAYNERLUC
    gitpan-cpan-maturity:     released

--- lib/Finance/Bank/Halifax/Sharedealing.pm
+++ lib/Finance/Bank/Halifax/Sharedealing.pm
@@ -12,11 +12,11 @@ Finance::Bank::Halifax::Sharedealing - access Halifax Sharedealing accounts from
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
